97835283079b910a56d8cebd86fa9642b5003522
Natzugen <Natzugen@users.noreply.github.com>
2015-10-03 18:16:42 -0500

    Update Commands.cpp

--- Server/WzAG/Commands.cpp
+++ Server/WzAG/Commands.cpp
@@ -1,51 +1,36 @@
 #include "StdAfx.h"
 
-// Custom Commands
-void MakeCommand(int aIndex,char* Message)
-{
-	if(!_strcmpi("/make",Message))
-	{
-		GCServerMsgStringSend("Usage: /make <type> <index> <lvl> <skill> <luck> <opt> <exc>",aIndex,1);
-		return;
-	}
 
-	if(strlen(Message) < 1)
-	{
-		GCServerMsgStringSend("Usage: /make <type> <index> <lvl> <skill> <luck> <opt> <exc>",aIndex,1);
-		return;
-	}
-
-	int Spaces = 0;
-
-	for(int i = 0; i < strlen(Message); i++)
-	{
-		if(Message[i] == ' ')
-		{
-			Spaces++;
-		}
-	}
-
-	if(Spaces < 6)
-	{
-		GCServerMsgStringSend("Please re-check the code string you gave.",aIndex,1);
-		return;
-	}
-
-	int ItemType,ItemNr,ItemLevel,ItemSkill,ItemLuck,ItemOpt,ItemExc;
-	sscanf(Message,"%d %d %d %d %d %d %d",&ItemType,&ItemNr,&ItemLevel,&ItemSkill,&ItemLuck,&ItemOpt,&ItemExc);
-
-	DWORD Item = ItemType * 512 + ItemNr;
-
-	ItemSerialCreateSend(aIndex,gObj[aIndex].MapNumber,gObj[aIndex].X,gObj[aIndex].Y,Item,ItemLevel,0,ItemSkill,ItemLuck,ItemOpt,aIndex,ItemExc,0);
-	
-	GCServerMsgStringSend("Item created successfully.",aIndex,1);
-}
 
 void InitCommands(int aIndex,unsigned char* Protocol)
 {
-	/*char CommandMake[] = "/make";
+	char CommandMake[] = "/Item";
+	
 	if(!memcmp(&Protocol[13],CommandMake,strlen(CommandMake)))
 	{
-		MakeCommand(aIndex,(char*)Protocol+13+strlen(CommandMake));
-	}*/
+		ChatDrop(aIndex,(char*)Protocol+13+strlen(CommandMake));
+	}
+}
+
+// Custom Commands
+void ChatDrop(DWORD gObjId, char* msg)
+{
+	OBJECTSTRUCT * gObj = (OBJECTSTRUCT*)OBJECT_POINTER(gObjId);
+
+	//========================================================================================================================
+	if (gObj->Authority != 32)
+	{
+		MsgOutput(gObjId, "[Item] : Command only for GM's");
+		return;
+	}
+
+	//========================================================================================================================
+
+	int ItemType = 0, ItemNr = 0, ItemLevel = 0, ItemSkill = 0, ItemLuck = 0, ItemOpt = 0, ItemExc = 0, ItemDur = 0, ItemAncient = 0;
+	sscanf_s(msg, "%d %d %d %d %d %d %d %d %d", &ItemType, &ItemNr, &ItemLevel, &ItemDur, &ItemSkill, &ItemLuck, &ItemOpt, &ItemExc, &ItemAncient);
+
+	DWORD Item = ItemType * 512 + ItemNr;
+
+	ItemSerialCreateSend(gObjId, gObj->MapNumber, gObj->X, gObj->Y, Item, ItemLevel, ItemDur, ItemSkill, ItemLuck, ItemOpt, gObjId, ItemExc, 0);
+	MsgOutput(gObjId, "[Item] : Created %d %d %d %d %d %d %d %d %d", ItemType, ItemNr, ItemLevel, ItemDur, ItemSkill, ItemLuck, ItemOpt, ItemExc, ItemAncient);
 }
